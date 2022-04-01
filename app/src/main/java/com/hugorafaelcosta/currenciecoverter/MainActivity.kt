c39775b67dac13dc0fa8ead65f9c5384f8e3cc89
Hugo Costa <costarafaelhugo@gmail.com>
2022-03-31 22:25:58 -0300

    Finalizado

---
+++ app/src/main/java/com/hugorafaelcosta/currenciecoverter/MainActivity.kt
@@ -0,0 +1,88 @@
+package com.hugorafaelcosta.currenciecoverter
+
+import android.os.Bundle
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.JsonObject
+import com.hugorafaelcosta.currenciecoverter.api.EndPoint
+import com.hugorafaelcosta.currenciecoverter.util.NetworkUtils
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var spFrom : Spinner
+    private lateinit var spTo : Spinner
+    private lateinit var btConvert : Button
+    private lateinit var tvResult : TextView
+    private lateinit var etValueFrom : EditText
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        spFrom = findViewById(R.id.spFrom)
+        spTo = findViewById(R.id.spTo)
+        btConvert = findViewById(R.id.btConvert)
+        tvResult = findViewById(R.id.tvResult)
+        etValueFrom = findViewById(R.id.etValueFrom)
+
+        getCurrencies()
+
+
+        btConvert.setOnClickListener { convertMoney() }
+    }
+
+    fun convertMoney(){
+        val retrofitClient = NetworkUtils.getRetrofitInstance("https://cdn.jsdelivr.net/")
+        val endpoint = retrofitClient.create(EndPoint::class.java)
+
+        endpoint.getCurreciesRate(spFrom.selectedItem.toString(), spTo.selectedItem.toString()).enqueue(object :
+            retrofit2.Callback<JsonObject>{
+            override fun onResponse(call: Call<JsonObject>, response: Response<JsonObject>) {
+                var data = response.body()?.entrySet()?.find { it.key == spTo.selectedItem.toString() }
+                val rate : Double = data?.value.toString().toDouble()
+                val conversion = etValueFrom.text.toString().toDouble() * rate
+
+                tvResult.setText(conversion.toString())
+            }
+
+            override fun onFailure(call: Call<JsonObject>, t: Throwable) {
+                println("Não foi")
+            }
+
+        })
+    }
+
+    fun getCurrencies(){
+        val retrofitClient = NetworkUtils.getRetrofitInstance("https://cdn.jsdelivr.net/")
+        val endpoint = retrofitClient.create(EndPoint::class.java)
+
+        endpoint.getCurrencies().enqueue(object : retrofit2.Callback<JsonObject> {
+            override fun onResponse(call: Call<JsonObject>, response: Response<JsonObject>) {
+                var data = mutableListOf<String>()
+
+                response.body()?.keySet()?.iterator()?.forEach {
+                    data.add(it)
+                }
+
+                val posBRL = data.indexOf("brl")
+                val posUSD = data.indexOf("usd")
+
+                val adapter = ArrayAdapter(baseContext, android.R.layout.simple_spinner_dropdown_item, data)
+                spFrom.adapter = adapter
+                spTo.adapter = adapter
+
+                spFrom.setSelection(posBRL)
+                spTo.setSelection(posUSD)
+            }
+
+            override fun onFailure(call: Call<JsonObject>, t: Throwable) {
+                println("Não foi")
+            }
+
+        })
+    }
+}
